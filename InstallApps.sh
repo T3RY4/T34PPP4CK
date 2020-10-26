64c0bbb94d2684ecdf0e41c7ceccb6eec5824ef5
t3ry4 <terya23419@gmail.com>
2020-10-27 01:42:32 +0200

    Update InstallApps.sh

--- InstallApps.sh
+++ InstallApps.sh
@@ -1,52 +1,109 @@
 #!/bin/bash
 adb devices
-adb root
+echo "Chrome [1/53]"
 adb install Chrome.apk
+echo "Magisk [2/53]"
 adb install Magisk.apk
+echo "Telegram [3/53]"
 adb install Telegram.apk
+echo "Clock [4/53]"
 adb install Clock.apk
+echo "Phone [5/53]"
 adb install Phone.apk
+echo "Contacts [6/53]"
 adb install Contacts.apk
+echo "Calendar [7/53]"
 adb install Calendar.apk
+echo "TickTick [8/53]"
 adb install TickTick.apk
+echo "LawnChair [9/53]"
 adb install LawnChair.apk
+echo "PowerPoint [10/53]"
 adb install PowerPoint.apk
+echo "Office [11/53]"
 adb install Office.apk
+echo "Excel [12/53]"
 adb install Excel.apk
+echo "EBook [13/53]"
 adb install EBook.apk
-adb install Acrobar.apk
+echo "Acrobat [14/53]"
+adb install Acrobat.apk
+echo "GooglePDF [15/53]"
 adb install GooglePDF.apk
+echo "Poweramp [16/53]"
 adb install PowerAMP.apk
+echo "Poweramp unlock [17/53]"
 adb install PowerAMPUnlocker.apk 
+echo "Messages [18/53]"
 adb install Messages.apk
+echo "Calculator [19/53]"
 adb install Calculator.apk
+echo "Spark [20/53]"
 adb install Spark.apk
+echo "Vodaphone [21/53]"
 adb install Vodaphone.apk
+echo "Privat24 [22/53]"
 adb install Privat24.apk
+echo "Photos [23/53]"
 adb install Photos.apk
-adb install ES.apk
+echo "MT [24/53]"
+adb install MT.apk
+echo "HedgeCam 2 [25/53]"
 adb install HedgeCam.apk
+echo "Snap Camera [26/53]"
 adb install SnapCamera.apk
+echo "Mcpro24fps [27/53]"
 adb install mc24.apk
+echo "Kde Connect [28/53]"
 adb install KDECon.apk
+echo "E-School [29/53]"
 adb install ESchool.apk
+echo "Minesweeper [30/53]"
 adb install Minesweeper.apk
+echo "Dia [31/53]"
 adb install Dia.apk
+echo "Prom.ua [32/53]"
 adb install Prom.apk
+echo "Aliexpress [33/53]"
 adb install Ali.apk
+echo "Github [34/53]"
 adb install Git.apk
+echo "Olx [35/53]"
 adb install Olx.apk
+echo "GPay [36/53]"
 adb install Gpay.apk
+echo "DroidCamX [37/53]"
 adb install DroidCamX.apk
+echo "4PDA [38/53]"
 adb install 4PDA.apk
+echo "NovaPoshta [39/53]"
 adb install NovaPoshta.apk
+echo "UkrPoshta [40/53]"
 adb install UkrPoshta.apk
+echo "Wikipedia Beta [41/53]"
 adb install WikiBeta.apk
+echo "Google Fit [42/53]"
 adb install GoogleFit.apk
+echo "YT Vanced [43/53]"
 adb install Vanced.apk
+echo "Family Link [44/53]"
 adb install FamilyLink.apk
+echo "1.1.1.1 [45/53]"
 adb install 1111.apk
+echo "Youtube music [46/53]"
 adb install YTMusic.apk
+echo "Tor [47/53]"
 adb install Tor.apk
+echo "Google keyboard [48/53]"
 adb install Gboard.apk
-
+echo "Viber [49/53]"
+adb install Viber.apk
+echo "Google Translate [50/53]"
+adb install Translator.apk
+echo "Google [51/53]"
+adb install Google.apk
+echo "VLC [52/53]"
+adb install VLC.apk
+echo "Simple Gallery [53/53]"
+adb install SimpleGallery.apk
+echo "DONE!!!"
