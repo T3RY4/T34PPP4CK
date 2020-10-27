12e0634c817c2671aadd73b98187e6f8ba62ea8e
t3ry4 <terya23419@gmail.com>
2020-10-27 22:01:03 +0200

    Update InstallApps.sh

--- InstallApps.sh
+++ InstallApps.sh
@@ -1,109 +1,111 @@
 #!/bin/bash
 adb devices
-echo "Chrome [1/53]"
+echo "Chrome [1/54]"
 adb install Chrome.apk
-echo "Magisk [2/53]"
+echo "Magisk [2/54]"
 adb install Magisk.apk
-echo "Telegram [3/53]"
+echo "Telegram [3/54]"
 adb install Telegram.apk
-echo "Clock [4/53]"
+echo "Clock [4/54]"
 adb install Clock.apk
-echo "Phone [5/53]"
+echo "Phone [5/54]"
 adb install Phone.apk
-echo "Contacts [6/53]"
+echo "Contacts [6/54]"
 adb install Contacts.apk
-echo "Calendar [7/53]"
+echo "Calendar [7/54]"
 adb install Calendar.apk
-echo "TickTick [8/53]"
+echo "TickTick [8/54]"
 adb install TickTick.apk
-echo "LawnChair [9/53]"
+echo "LawnChair [9/54]"
 adb install LawnChair.apk
-echo "PowerPoint [10/53]"
+echo "PowerPoint [10/54]"
 adb install PowerPoint.apk
-echo "Office [11/53]"
+echo "Office [11/54]"
 adb install Office.apk
-echo "Excel [12/53]"
+echo "Excel [12/54]"
 adb install Excel.apk
-echo "EBook [13/53]"
+echo "EBook [13/54]"
 adb install EBook.apk
-echo "Acrobat [14/53]"
+echo "Acrobat [14/54]"
 adb install Acrobat.apk
-echo "GooglePDF [15/53]"
+echo "GooglePDF [15/54]"
 adb install GooglePDF.apk
-echo "Poweramp [16/53]"
+echo "Poweramp [16/54]"
 adb install PowerAMP.apk
-echo "Poweramp unlock [17/53]"
+echo "Poweramp unlock [17/54]"
 adb install PowerAMPUnlocker.apk 
-echo "Messages [18/53]"
+echo "Messages [18/54]"
 adb install Messages.apk
-echo "Calculator [19/53]"
+echo "Calculator [19/54]"
 adb install Calculator.apk
-echo "Spark [20/53]"
+echo "Spark [20/54]"
 adb install Spark.apk
-echo "Vodaphone [21/53]"
+echo "Vodaphone [21/54]"
 adb install Vodaphone.apk
-echo "Privat24 [22/53]"
+echo "Privat24 [22/54]"
 adb install Privat24.apk
-echo "Photos [23/53]"
+echo "Photos [23/54]"
 adb install Photos.apk
-echo "MT [24/53]"
+echo "MT [24/54]"
 adb install MT.apk
-echo "HedgeCam 2 [25/53]"
+echo "HedgeCam 2 [25/54]"
 adb install HedgeCam.apk
-echo "Snap Camera [26/53]"
+echo "Snap Camera [26/54]"
 adb install SnapCamera.apk
-echo "Mcpro24fps [27/53]"
+echo "Mcpro24fps [27/54]"
 adb install mc24.apk
-echo "Kde Connect [28/53]"
+echo "Kde Connect [28/54]"
 adb install KDECon.apk
-echo "E-School [29/53]"
+echo "E-School [29/54]"
 adb install ESchool.apk
-echo "Minesweeper [30/53]"
+echo "Minesweeper [30/54]"
 adb install Minesweeper.apk
-echo "Dia [31/53]"
+echo "Dia [31/54]"
 adb install Dia.apk
-echo "Prom.ua [32/53]"
+echo "Prom.ua [32/54]"
 adb install Prom.apk
-echo "Aliexpress [33/53]"
+echo "Aliexpress [33/54]"
 adb install Ali.apk
-echo "Github [34/53]"
+echo "Github [34/54]"
 adb install Git.apk
-echo "Olx [35/53]"
+echo "Olx [35/54]"
 adb install Olx.apk
-echo "GPay [36/53]"
+echo "GPay [36/54]"
 adb install Gpay.apk
-echo "DroidCamX [37/53]"
+echo "DroidCamX [37/54]"
 adb install DroidCamX.apk
-echo "4PDA [38/53]"
+echo "4PDA [38/54]"
 adb install 4PDA.apk
-echo "NovaPoshta [39/53]"
+echo "NovaPoshta [39/54]"
 adb install NovaPoshta.apk
-echo "UkrPoshta [40/53]"
+echo "UkrPoshta [40/54]"
 adb install UkrPoshta.apk
-echo "Wikipedia Beta [41/53]"
+echo "Wikipedia Beta [41/54]"
 adb install WikiBeta.apk
-echo "Google Fit [42/53]"
+echo "Google Fit [42/54]"
 adb install GoogleFit.apk
-echo "YT Vanced [43/53]"
+echo "YT Vanced [43/54]"
 adb install Vanced.apk
-echo "Family Link [44/53]"
+echo "Family Link [44/54]"
 adb install FamilyLink.apk
-echo "1.1.1.1 [45/53]"
+echo "1.1.1.1 [45/54]"
 adb install 1111.apk
-echo "Youtube music [46/53]"
+echo "Youtube music [46/54]"
 adb install YTMusic.apk
-echo "Tor [47/53]"
+echo "Tor [47/54]"
 adb install Tor.apk
-echo "Google keyboard [48/53]"
+echo "Google keyboard [48/54]"
 adb install Gboard.apk
-echo "Viber [49/53]"
+echo "Viber [49/54]"
 adb install Viber.apk
-echo "Google Translate [50/53]"
+echo "Google Translate [50/54]"
 adb install Translator.apk
-echo "Google [51/53]"
+echo "Google [51/54]"
 adb install Google.apk
-echo "VLC [52/53]"
+echo "VLC [52/54]"
 adb install VLC.apk
-echo "Simple Gallery [53/53]"
+echo "Simple Gallery [53/54]"
 adb install SimpleGallery.apk
+echo "GMaps [54/54]"
+adb install Maps.apk
 echo "DONE!!!"
